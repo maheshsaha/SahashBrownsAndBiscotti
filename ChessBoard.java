1ca9bfb803b73bce9d091aa9d19a13ed60e4f2b3
Mahesh Saha <sahamahesh20@gmail.com>
2017-01-05 14:42:50 -0500

    javaDoc style comments added for ChessBoard.java

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,13 +1,24 @@
 public class ChessBoard {
+    // Default pieces for Chess with unique values so names can be referenced 
+    // in the bitboard
     public static final int PAWN = 0;
     public static final int BISHOP = 1;
     public static final int KNIGHT = 2;
     public static final int ROOK = 3;
     public static final int QUEEN = 4;
     public static final int KING = 5;
+
+    // String Array containing abbreviations for the chess pieces
     public static final String[] names = new String[] {"P", "B", "H", "R", "Q", "K"};
+
+    // Two bitboard arrays for each color, storing piece location
     public final long[] bbWhite;
     public final long[] bbBlack;
+
+    /**
+     *Default constructor 
+     *Initializes the default board layout
+     */    
 
     public ChessBoard(){
 	bbWhite = new long[6];
@@ -20,6 +31,11 @@ public class ChessBoard {
 	bbBlack = new long[6];
 	for (int i=0; i<bbBlack.length; i++) bbBlack[i]=Long.reverse(bbWhite[i]);
     }
+    
+    /**
+     *toString method that returns the chess board 
+     *@return String Visual representation of the present chessboard. The lowercase letter preceeding the uppercase letter indicates color. K = King, Q = Queen, R = Rook, B = Bishop, N = Knight, P = Pawn
+     */    
     
     public String toString() {
 	String out = "";
@@ -35,17 +51,32 @@ public class ChessBoard {
 	return out;
     }
 
+    /**
+     *This method is used to get a bitboard that contains only white pieces 
+     *@return long A bitboard containing present white pieces only
+     */    
+
     public long getWhite() {
 	long out = 0L;
 	for (int i=0; i<6; i++) out |= bbWhite[i];
 	return out;
     }
 
+    /**
+     *This method is used to get a bitboard that contains only black pieces 
+     *@return long A bitboard containing present black pieces only
+     */
+
     public long getBlack() {
 	long out = 0L;
 	for (int i=; i<6; i++) out |= bbBlack[i];
 	return out;
     }
+
+    /**
+     *This method is used to get a bitboard containg all of the pieces 
+     *@return long A bitboard containing all present pieces
+     */
 
     public long getAll() {
 	return getWhite() | getBlack();
