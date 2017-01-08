0cc3f008b61ea0568a5b6d107abdec2055d5c191
JTurcotti <turcotti.joshua@gmail.com>
2017-01-08 00:44:30 -0500

    Added bitwise magic for bishop/rook/queen move generation

--- Chess.java
+++ Chess.java
@@ -1,18 +1,34 @@
 public class Chess {
     //never meant to be inititated, no instance methods.
 
+    //diagonal masks
+    public static long[] rowMasks = new long[64];
+    public static long[] columnMasks = new long[64];
+    //parallel masks
+    public static long[] forwardMasks = new long[64];
+    public static long[] backwardMasks = new long[64];
+
+    public static long[][] masks = new long[][] {rowMasks, forwardMasks, columnMasks, backwardMasks};
+    
+    //piece masks
     public static long[] bishopMasks = new long[64];
     public static long[] rookMasks = new long[64];
     public static long[] queenMasks = new long[64];
+    public static long[] knightMasks = new long[64];
+    public static long[] kingMasks = new long[64];
+    public static long[] pawnMasks = new long[64];
+    
     static {
 	for (int i=0; i<64; i++) {
-	    bishopMasks[i]=diagonalMask(i);
-	    rookMasks[i]=parallelMask(i);
+	    diagonalMask(i);
+	    parallelMask(i);
+	    bishopMasks[i]=forwardMasks[i] | backwardMasks[i];
+	    rookMasks[i]=rowMasks[i] | columnMasks[i];
 	    queenMasks[i]=bishopMasks[i] | rookMasks[i];
 	}
     }
 	
-    private static long diagonalMask(int position) {
+    private static void diagonalMask(int position) {
 	int forward  = position%8 - position/8;
 	int backward = position%8 + position/8;
 	
@@ -27,14 +43,17 @@ public class Chess {
 	for (int i=0; i<(8-Math.abs(backward-7)); i++) {
 	    backwardMask += 1L<<(7*i+start);
 	}
-	return (forwardMask | backwardMask) & ~(1L<<position);
+	
+	forwardMasks[position] = forwardMask - (1L<<position);
+	backwardMasks[position] = backwardMask - (1L<<position);
     }
 
-    private static long parallelMask(int position) {
+    private static void parallelMask(int position) {
 	long horizontalMask = ((1L<<8)-1L)<<(position/8*8);
 	long verticalMask = (1L<<position%8)*72340172838076673L;
 
-	return (horizontalMask | verticalMask) & ~(1L<<position);
+	rowMasks[position] = horizontalMask - (1L<<position);
+	columnMasks[position] = verticalMask - (1L<<position);
     }
 
 
@@ -42,19 +61,33 @@ public class Chess {
     public static String longToString(long l) {
 	String out = "";
 	for (int i=63; i>=0; i--) {
-	    String s = "_";
-	    if (((1L << i) & l) != 0) s="O";
+	    String s = "0";
+	    if (((1L << i) & l) != 0) s="1";
 	    out+=s;
 	    if (i%8==0) out +="\n";
 	}
 	return out;
     }
+
+    public static long forwardAttacks(long occ, int direction, int i) {
+	long mask = masks[direction%4][i];
+	long moves = occ & mask;
+	long reverse = Long.reverse(moves);
+	moves -= 2 * (1L<<i);
+	reverse -= 2* (1L<<(63-i));
+	moves ^= Long.reverse(reverse);
+	return moves & mask;
+    }
     
     public static void main(String[] args) {
-	System.out.println(longToString(queenMasks[Integer.parseInt(args[0])]));
+	long occ = 0b00000000_01000001_00000010_00000000_00001000_00010010_00100000_01000000L;
+	prl(forwardAttacks(occ, 3, 27));
     }
 
     static void pr(Object o) {
 	System.out.println(o);
+    }
+    static void prl(long l) {
+	pr(longToString(l));
     }
 }	    
